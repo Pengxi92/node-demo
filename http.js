c17423e8b8db1ceb47f3bf1a2e602aa48686e6d5
xi.peng <xi.peng@wormpex.com>
2019-02-26 21:00:57 +0800

    添加url参数获取

--- http.js
+++ http.js
@@ -1,9 +1,17 @@
 var http = require('http');
+var url = require("url");
 
 http.createServer(function(req, res) {
+  var pathname = url.parse(req.url).pathname;
+
+  console.log(req.url, req.url.indexOf('80'));
+  if (req.url.indexOf('start') !== -1) {
+    console.log(req.url, url.parse(req.url), url.parse(req.url).pathname);
+  }
+  
   res.writeHead(200, {'Content-Type': 'text/plain'});
 
   res.end('hello world');
-}).listen(8009);
+}).listen(8010);
 
-console.log('Server running at http://127.0.0.1:8009/');
+console.log('Server running at http://127.0.0.1:8010/');
